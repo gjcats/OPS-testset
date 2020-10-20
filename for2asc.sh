8701dd2a06ffe1249c8c85bd62948fa7a6e00368
Gerard Cats <69036105+gjcats@users.noreply.github.com>
2020-10-20 16:56:15 +0200

    The unformatted *.ops files, converted to ascii
    The zip file contains the Fortran unformatted ops files provided by RIVM, in ascii format. Unzip and convert them back to Fortran unformatted files before you run OPS. This script asc2for.sh is there to assist you in that back conversion.
    
    The script for2asc.sh has been adapted to handle the new multitude of ops unformatted files (146 at the time of writing) automatically.  Until RIVM change their files again, you will not need to use this script, because its output is already in opsINasc.zip.

--- for2asc.sh
+++ for2asc.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-# Gerard Cats, 23 August 2020
-base=`basename -s .ops $1`
-echo to create $base.asc
+# Gerard Cats, 20 October 2020
+
+rivm=/drives/d/Applics/OPS-Pro_2020/Data
+
 mkdir $$
-cp $1 $$/fort.1
-gfortran for2asc.f90 -o for2asc.exe
-(cd $$; ../for2asc.exe)
-mv $$/fort.2 $base.asc
+cd $$
+gfortran ../for2asc.f90 -o for2asc.exe
+
+for file in $rivm/*.ops; do
+   if [[ `file $file` =~ data ]] ; then
+      base=`basename -s .ops $file`
+      echo to create $base.asc
+      ln -s $file fort.1
+      ln -s $base.asc fort.2
+      ./for2asc.exe
+      rm fort.*
+   fi
+done
+
+zip ../opsINasc.zip *.asc
+cd ..
 rm -rf $$
